1fc2c73b38a50fccdf6290442d77b96862ab9466
Rakib Uddin <rsrakib404@gmail.com>
2022-03-14 17:37:49 +0600

    http service updated

--- lib/constants/http_service/http_service.dart
+++ lib/constants/http_service/http_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class HttpService {
   static Future<List<ItemModel>> fetchItem(int page, int limit) async {
-    const _baseUrl = ' https://picsum.photos/v2/list ';
+    const _baseUrl = 'https://picsum.photos/v2/list';
 
     final response =
         await http.get(Uri.parse('$_baseUrl?page=$page&limit=$limit'));
